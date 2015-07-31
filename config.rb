94f589c926c3282dd3a763a54344797a9d4e5ec1
Danko-M <dankomilutinovic@gmail.com>
2015-07-31 13:15:20 +0200

    bootstrap starter

---
+++ config.rb
@@ -0,0 +1,21 @@
+preferred_syntax = :sass
+http_path = ''
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'img'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+
+
+# 3. You can select your preferred output style here (can be overridden via the command line):
+#output_style = :expanded
+
+# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
+# output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# don't touch this
+preferred_syntax = :scss
