c568f69c3140a1a2dd7477389df2f3cc012e5ba3
Danko-M <dankomilutinovic@gmail.com>
2015-08-06 18:39:49 +0200

    testimonial data added via mustache.js library (js templates)

--- js/main.js
+++ js/main.js
@@ -4,37 +4,63 @@ $(function() {
 	  offset: 50,
 	  target: ".navbar"
 	});
+  
+  
+  
+  
 
 	var smoothScrollLinks = $('.collapse a[href*=#]:not([href=#]), #about a');
 
-  $(smoothScrollLinks).click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+    $(smoothScrollLinks).click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
-  });
-
-  if ($(window).width() > 750) {
-	  $('.client:even' ).addClass('pull-left');
-	  $('.client:odd' ).addClass('pull-right');  	
-  }
-
-
-/*   //  for testing
-  jQuery(window).load(function () {
-      var width = jQuery(window).width();
-      var height = jQuery(window).height();
-      var documentWidth = jQuery(document).width();
-      var documentHeight = jQuery(document).height();
-      console.log(width);
-      console.log(height);
     });
-*/
+  
+  
+  
+
+
+  $.getJSON('../data/testimonials.json', function(data) {
+
+    var template = $('#testimonialstpl').html();
+    var html = Mustache.to_html(template, data);
+    $('#carousel-testimonials .carousel-inner').html(html);
+    
+    var testimonialsItems = $('#testimonials .item').length;
+    for(var i =0; i < testimonialsItems; i++){
+      var insertText = '<li data-target="#carousel-testimonials" data-slide-to="' + i + '"></li>';
+      $('#carousel-testimonials ol').append(insertText);
+    }  
+    
+    $('#carousel-testimonials .item:first').addClass('active');
+    $('#carousel-testimonials li:first').addClass('active');
+    
+    if ($(window).width() > 750) {
+        $('.client:even' ).addClass('pull-left');
+        $('.client:odd' ).addClass('pull-right');  	
+    }
+    
+  });
+  
+  
+  
+  
+    var portfolioItems = $('#carousel-portfolio .item').length;
+    for(var i =0; i < portfolioItems; i++){
+      var insertText = '<li data-target="#carousel-portfolio" data-slide-to="' + i + '"></li>';
+      $('#carousel-portfolio ol').append(insertText);
+    }  
+    
+    $('#carousel-portfolio .item:first').addClass('active');
+    $('#carousel-portfolio li:first').addClass('active');
+
 
 });
